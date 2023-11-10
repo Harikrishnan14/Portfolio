8559daf5929ed8fc62543da28bb6958538610e5a
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-11-10 23:45:33 +0530

    Third commit

--- src/Components/Assets/data/projects.js
+++ src/Components/Assets/data/projects.js
@@ -1,8 +1,52 @@
 import eNotebook from '../images/eNotebook-Logo.png'
 import NewsApp from '../images/NewsApp-Logo.png'
 import TextUtils from '../images/TextUtils-Logo.png'
+import Quora from '../images/Quora-Logo.png'
+import OMTB from '../images/OnlineMovieTicketBooking-Logo.png'
+
+import GDGFinder from '../images/GDGFinder-Logo.png'
+import DevByte from '../images/DevByte-Logo.png'
+import MarsReatEstate from '../images/MarsRealEstate-Logo.png'
+import TrackMySleep from '../images/SleepTracker-Logo.png'
+import GuessIt from '../images/GuessIt-Logo.png'
+import DessertPusher from '../images/DessertPusher-Logo.png'
+import Trivia from '../images/TriviaApp-Logo.png'
+import ColorMyViews from '../images/ColorMyViews-Logo.png'
+import DiceRoller from '../images/DiceRoller-Logo.png'
+
+import FER from '../images/FER-Logo.png'
 
 export const projects = [
+    {
+        id: 13,
+        title: "FER For Paralyzed Patients",
+        date: "",
+        description:
+            "Face Emotion Recognition System tailored for paralyzed patients, employing advanced computer vision algorithms. Enabled individuals with limited mobility to communicate emotions effectively, fostering a more inclusive and responsive healthcare environment. Demonstrated expertise in artificial intelligence, programming, and empathetic technology design.",
+        image: FER,
+        tags: [
+            "Machine Learning",
+            "Python",
+            "Arduino",
+            "Android Studio",
+        ],
+        category: "machine learning",
+        github: "https://github.com/AsHwInSkV/Face-Emotion-Recognition-System-For-Paralyzed-Patients.git",
+    },
+    {
+        id: 4,
+        title: "GDG Finder",
+        date: "",
+        description:
+            "GDG Finder is a cutting-edge mobile application using Kotlin to streamline the process of locating Google Developer Groups (GDGs) worldwide. Leveraging Kotlin's concise and expressive syntax, the project involved implementing geolocation services, integrating APIs for real-time data retrieval, and ensuring a seamless user experience. Successfully delivered a user-friendly solution that enhances the accessibility of GDGs, contributing to the global developer ecosystem.",
+        image: GDGFinder,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/GDG-Finder-App.git",
+    },
     {
         id: 0,
         title: "eNotebook",
@@ -47,6 +91,152 @@ export const projects = [
         ],
         category: "web app",
         github: "https://github.com/Harikrishnan14/TextUtils-React.git",
+    },
+    {
+        id: 3,
+        title: "Quora - Clone",
+        date: "",
+        description:
+            "Quora - Clone is a robust and functional blog application using Django, with a primary emphasis on enhancing core functionalities rather than design aesthetics. Leveraged Django's powerful features to implement a seamless user experience, focusing on features such as user authentication, content creation, and comment management. Prioritized functionality and performance optimization to create a highly responsive and efficient blog platform, showcasing a deep understanding of backend development and Django framework intricacies.",
+        image: Quora,
+        tags: [
+            "HTML",
+            "CSS",
+            "Python - Django",
+            "Bootstrap",
+        ],
+        category: "web app",
+        github: "https://github.com/Harikrishnan14/Quora-Django.git",
+    },
+    {
+        id: 5,
+        title: "Dev Byte Viewer",
+        date: "",
+        description:
+            "Dev Byte Viewer is a dynamic and user-friendly application built entirely in Kotlin. Leveraging my expertise in Kotlin, I implemented key features that enhance the user experience and streamline content consumption for developers. The app serves as a comprehensive platform for viewing and sharing bite-sized development tutorials, showcasing my proficiency in both mobile app development and Kotlin programming.",
+        image: DevByte,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Dev-Byte-Viewer-App.git",
+    },
+    {
+        id: 6,
+        title: "Mars Real Estate",
+        date: "",
+        description:
+            "This app allows users to explore virtual properties on Mars, providing a unique and immersive experience. Through strategic use of Kotlin, I optimized performance and ensured a smooth user interface, contributing to the success of the project and highlighting my skills in modern mobile application development.",
+        image: MarsReatEstate,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Mars-Real-Estate-App.git",
+    },
+    {
+        id: 7,
+        title: "Track My Sleep Quality",
+        date: "",
+        description:
+            "This project involved creating a user-friendly interface to monitor and analyze sleep patterns, providing valuable insights into sleep quality. The use of Kotlin facilitated efficient code development and maintenance, contributing to the overall success of the application in providing users with valuable insights into their sleep habits.",
+        image: TrackMySleep,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Track-My-Sleep-Quality-App.git",
+    },
+    {
+        id: 8,
+        title: "Guess It",
+        date: "",
+        description:
+            "Guess It App showcases my expertise in Kotlin, offering a dynamic and engaging guessing game experience. Built with precision and creativity, the project seamlessly blends user-friendly design with robust functionality. Users enjoy a captivating interface while challenging their intellect. This app represents my commitment to delivering innovative solutions through Kotlin, highlighting my proficiency in mobile app development.",
+        image: GuessIt,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Guess-It-App.git",
+    },
+    {
+        id: 14,
+        title: "Online Movie Ticket Booking",
+        date: "",
+        description:
+            "Online Movie Ticket Booking system is a full stack web application streamlining the ticketing process for enhanced user experience. Leveraged strong analytical and problem-solving skills to ensure seamless functionality, optimizing the platform for both customers and administrators. Demonstrated proficiency in web development and database management, contributing to the project's success.",
+        image: OMTB,
+        tags: [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "PHP",
+            "MySQL",
+            "XAMPP",
+        ],
+        category: "web app",
+        github: "https://github.com/Harikrishnan14/Online-Movie-Ticket-Booking-System.git",
+    },
+    {
+        id: 10,
+        title: "Trivia",
+        date: "",
+        description:
+            "Trivia App seamlessly blends innovation and engagement. Through meticulous coding and intuitive design, I've transformed the concept of trivia into a dynamic and user-friendly experience. The app's sleek interface and robust functionality showcase my proficiency in Kotlin, making it a testament to my expertise in mobile app development.",
+        image: Trivia,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Trivia-App.git",
+    },
+    {
+        id: 11,
+        title: "Color My Views",
+        date: "",
+        description:
+            "Color My Views App is a dynamic Android app powered by Kotlin, designed to enhance user interface customization. With intuitive functionality, it seamlessly transforms views with vibrant hues, allowing users to personalize their app experience. This project showcases my proficiency in Kotlin and my commitment to creating visually appealing, user-centric applications.",
+        image: ColorMyViews,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/ColorMyViews-App.git",
+    },
+    {
+        id: 12,
+        title: "Dice Roller",
+        date: "",
+        description:
+            "Elevating randomness with precision, my Dice Roller App, crafted in Kotlin, seamlessly transforms decision-making. Harnessing the power of elegant code, it delivers a sleek and intuitive interface, ensuring an immersive and reliable dice-rolling experience. Precision meets playfulness, as this app adds a touch of innovation to the age-old randomness of dice, making it an engaging and efficient tool for any tabletop adventure or impromptu decision-making.",
+        image: DiceRoller,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Dice-Roller-App.git",
+    },
+    {
+        id: 9,
+        title: "Dessert Pusher",
+        date: "",
+        description:
+            "Dessert Pusher App is a user-friendly mobile application using Kotlin. The app allows users to effortlessly select their preferred desserts, with the total amount dynamically updating in real-time. One of the key features of the app is its robust handling of device orientation changes. I implemented a seamless solution to ensure that data persistence is maintained when users rotate their devices.",
+        image: DessertPusher,
+        tags: [
+            "Kotlin",
+            "Android Studio",
+        ],
+        category: "android app",
+        github: "https://github.com/Harikrishnan14/Dessert-Pusher-App.git",
     },
 
 ];
