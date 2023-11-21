228759cfb62eb527e7c050343d916c0afd3a28fe
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-11-21 23:50:47 +0530

    Changes on Navbar & Skills section + Some Responsive behaviour

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -12,18 +12,20 @@ const Skills = () => {
           <div className='skill' key={m_id}>
             <h2 className='skills-item-title'>{item.title}</h2>
             <div className="skill-list">
-              {item.skills.map( (skill, id) => (
+              {item.skills.map((skill, id) => (
                 <div className="skill-item" key={id}>
                   <img className='skill-img' src={skill.image} alt="" />
-                  {skill.name}
+                  <div className="skillname">
+                    {skill.name}
+                  </div>
                 </div>
               ))}
             </div>
           </div>
         ))}
-        
+
       </div>
-      
+
     </section>
   )
 }
