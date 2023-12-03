6fd5203f41dc3187de0c39deeb9e749555c3aebb
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-12-03 21:14:32 +0530

    Updates on links

--- src/Components/Assets/data/links.js
+++ src/Components/Assets/data/links.js
@@ -1,6 +1,6 @@
 export const links = {
     Github: "https://github.com/Harikrishnan14",
-    Resume: "https://drive.google.com/file/d/1XVeexf1c6nXE77zdW0omc38aAYHUKPVG/view?usp=sharing",
+    Resume: "https://drive.google.com/file/d/12a8H7UOdaQWZHddNJegK7YFDDPLT1FGl/view?usp=sharing",
     Linkedin: "https://www.linkedin.com/in/harikrishnan14",
     Instagram: "https://www.instagram.com/_the_wicked_one_",
     Whatsapp: "https:/wa.me/+919496524721",
